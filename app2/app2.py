35180b4ae16c902125b5b2f63b4b01540dccf101
Jakub Pietraszek <yaaqbp@gmail.com>
2023-12-11 22:30:08 +0100

    sample python apps with low sense

---
+++ app2/app2.py
@@ -0,0 +1,11 @@
+# app2/app2.py
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return 'Hello from App 2!'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5001)
