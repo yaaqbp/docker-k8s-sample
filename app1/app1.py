b0d0cea8bd87152806c45608db582befb053d5ca
Jakub Pietraszek <yaaqbp@gmail.com>
2023-12-12 13:08:38 +0100

    sample app in two container - works with docker compose, next step is to build working conf in k8s

--- app1/app1.py
+++ app1/app1.py
@@ -1,3 +1,6 @@
+
+"""
+    
 # app1/app1.py
 from flask import Flask
 
@@ -6,6 +9,22 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return 'Hello from App 1!'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+
+
+
+"""
+
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/api/data', methods=['GET'])
+def get_data():
+    data = {'message': 'Hello from App1!'}
+    return jsonify(data)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
--- app2/app2.py
+++ app2/app2.py
@@ -1,3 +1,4 @@
+"""
 # app2/app2.py
 from flask import Flask
 
@@ -9,3 +10,25 @@ def hello():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001)
+
+
+"""
+
+import requests
+from flask import Flask
+#render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def display_data():
+    api_url = 'http://app1:5000/api/data'  # 'app1' to nazwa kontenera z app1 w Docker Compose
+    response = requests.get(api_url)
+    data = response.json()
+    #return render_template('index.html', message=data['message'])
+    return f"from app1 API: {data['message']}"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5001)
+
+
