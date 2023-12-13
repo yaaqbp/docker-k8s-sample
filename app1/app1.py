27a5849219c5f4d9bda6fe21053cc99dd8a871de
Jakub Pietraszek <yaaqbp@gmail.com>
2023-12-13 18:01:00 +0100

    working authentication in secrey yaml added - using it just like 'os.getenv' causes problems with http header, so it is send as base64 and checked the same way

--- app1/app1.py
+++ app1/app1.py
@@ -1,30 +1,59 @@
 
-"""
-    
-# app1/app1.py
-from flask import Flask
+from flask import Flask, jsonify, request
+import os
+import base64
 
 app = Flask(__name__)
+def check_credentials(username, password):
+   
+    #k8s vars:
+    expected_username = base64.b64encode(os.getenv("USERNAME").encode("ascii")).decode('ascii')
+    expected_password = base64.b64encode(os.getenv("PASSWORD").encode("ascii")).decode('ascii')
+    #docker vars:
+    #expected_username = 'api-username'
+    #expected_password = 'api-password'
+    return username == expected_username and password == expected_password
 
-@app.route('/')
-def hello():
-    return 'Hello from App 1!'
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
-
-
-
-"""
-
-from flask import Flask, jsonify
-
-app = Flask(__name__)
 
 @app.route('/api/data', methods=['GET'])
 def get_data():
-    data = {'message': 'Hello from App1!'}
+    
+    try:
+
+        auth_header = request.headers.get('Authorization')
+        username, password = auth_header.split(':')
+        if check_credentials(username, password):
+            data = {'message': 'Hello from App1 with credentials!'}
+        else:
+            data = {'message': f'Wrong credentials get from app2: {username} {password}, expected from secret: {expected_username} {expected_password}, '}
+
+    except Exception as e:
+        data = {'message': 'app1 error' + str(e)}
     return jsonify(data)
+    
+    #data = {'message': 'app1 success'}
+    #return jsonify(data)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
+
+
+"""
+
+    try:
+        
+        expected_username = os.environ.get("USERNAME")
+        expected_password = os.environ.get("PASSWORD")
+        #expected_username = 'api-username'
+        #expected_password = 'api-password'
+
+        #auth_header = request.headers.get('Authorization')
+        #username, password = auth_header.split(':')
+        if str(expected_username) == str(username) and \
+            str(expected_password) == str(password):
+            data = {'message': 'Hello from App1!'}
+        else:
+            data = {'message': f'Wrong credentials get from app2: {username} {password}, expected from secret: {expected_username} {expected_password}, '}
+    except Exception as e:
+        data = {'message': 'app1 error' + str(e)}
+"""
--- app2/app2.py
+++ app2/app2.py
@@ -1,21 +1,10 @@
-"""
-# app2/app2.py
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route('/')
-def hello():
-    return 'Hello from App 2!'
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001)
-
-
-"""
 
 import requests
 from flask import Flask
+import os
+import base64
+from kubernetes import client, config
+
 #render_template
 
 app = Flask(__name__)
@@ -23,16 +12,30 @@ app = Flask(__name__)
 @app.route('/')
 def display_data():
     try:
-        #api_url= 'http://app1:5000/api/data'  # 'app1' to nazwa kontenera z app1 w Docker Compose
+
+        #k8s vars:
+        
         api_url = 'http://myapp-service:5000/api/data'  # 'myapp-service' odwołuje się do app1 korzystając z k8s
-        response = requests.get(api_url)
+        username = base64.b64encode(os.getenv("USERNAME").encode("ascii")).decode('ascii')
+        password = base64.b64encode(os.getenv("PASSWORD").encode("ascii")).decode('ascii')
+        #username = 'api-username'
+        #password = 'api-password'
+        """
+        #docker vars:
+        api_url= 'http://app1:5000/api/data'  # 'app1' to nazwa kontenera z app1 w Docker Compose
+        username = 'api-username'
+        password = 'api-password'
+        """
+
+        headers = {'Authorization': username + ':' + password}
+        response = requests.get(api_url, headers=headers)
         data = response.json()
         #return render_template('index.html', message=data['message'])
-        return f"from app1 API: {data['message']}"
+        #return f"from app1 API with credentials: {data['message']}"
+        return f'username: {username}, type of username {type(username)==str}, password: {password}, type of password {type(password)==str}, message: {data["message"]}'
     except Exception as e:
-        return str(e)
+        return "from app2 "+str(e)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001)
-
 
