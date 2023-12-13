cc672468427656f2cbd39ef950183cc9e241aad6
Jakub Pietraszek <yaaqbp@gmail.com>
2023-12-13 09:29:18 +0100

    created working yaml for k8s and README how to use that

--- README.md
+++ README.md
@@ -1,6 +1,16 @@
 # docker-k8s-sample
 Current work:
-pull repo
-1. "docker compose build"
-2. "docker compose up"
-3. go to localhost:5001 - there is visible data that app2 get from app1 
+0. pull repo
+
+to check work in docker:
+1. change "api_url" variable in app2.py to use 'app1' in http
+2. "docker compose build"
+3. "docker compose up"
+4. go to localhost:5001 - there is visible data that app2 get from app1 
+
+to use in k8s:
+it was created using minikube:
+1. minikube start
+2. minikube tunnel (in other terminal, has to work all the time)
+3. kubectl apply -f ./kubernetes
+4. go to localhost:5001 - there is visible data that app2 get from app1 
--- app2/app2.py
+++ app2/app2.py
@@ -22,11 +22,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def display_data():
-    api_url = 'http://app1:5000/api/data'  # 'app1' to nazwa kontenera z app1 w Docker Compose
-    response = requests.get(api_url)
-    data = response.json()
-    #return render_template('index.html', message=data['message'])
-    return f"from app1 API: {data['message']}"
+    try:
+        #api_url= 'http://app1:5000/api/data'  # 'app1' to nazwa kontenera z app1 w Docker Compose
+        api_url = 'http://myapp-service:5000/api/data'  # 'myapp-service' odwołuje się do app1 korzystając z k8s
+        response = requests.get(api_url)
+        data = response.json()
+        #return render_template('index.html', message=data['message'])
+        return f"from app1 API: {data['message']}"
+    except Exception as e:
+        return str(e)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001)
